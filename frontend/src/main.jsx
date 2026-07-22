ca91f5b5254b13fe0608f217325d974e7d41a21e
Om Navale <omnavale899@gmail.com>
2026-07-22 23:49:26 +0530

    Frontend Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// This is the very first JS file that runs. It finds the <div id="root">
-// in index.html and renders our whole <App /> component tree into it.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
